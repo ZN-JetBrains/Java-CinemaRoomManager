0dd95a5226741753d9bc0e6e2a763cd819f7d682
Zaid Neurothrone <zaid_neurothrone@hotmail.com>
2021-02-01 22:25:49 +0100

    Complete Stage 2/5: Sold!

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,8 +1,43 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Cinema
 {
     public static void main(String[] args)
+    {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        final int ROWS = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter the number of seats in each row:");
+        final int COLUMNS = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Total income:");
+
+        final int TOTAL_SEATS = ROWS * COLUMNS;
+        final int PRICE_PER_TICKET_FRONT = 10;
+        final int PRICE_PER_TICKET_BACK;
+
+        final int TOTAL_REVENUE;
+
+        final int frontRows = ROWS / 2;
+        final int backRows = ROWS - frontRows;
+
+        if (TOTAL_SEATS < 60)
+        {
+            TOTAL_REVENUE = TOTAL_SEATS * PRICE_PER_TICKET_FRONT;
+        }
+        else
+        {
+            PRICE_PER_TICKET_BACK = 8;
+            TOTAL_REVENUE = COLUMNS * (PRICE_PER_TICKET_FRONT * frontRows) + COLUMNS * (PRICE_PER_TICKET_BACK * backRows);
+        }
+
+        System.out.println("$" + TOTAL_REVENUE);
+    }
+
+    private void printArrangement()
     {
         final int ROW_SIZE = 7;
         final int COL_SIZE = 8;
